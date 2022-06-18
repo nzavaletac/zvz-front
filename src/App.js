54df3c55b99ae0cc43d6643c9d735eed9c8b16bf
Noe Zavaleta C <nzavaletac@gmail.com>
2022-06-17 22:43:32 -0500

    add facebook messenger

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+      <MessengerCustomerChat pageId="109544380864461" appId="476838759574500" />
+      ,
+    </>
   );
 }
 
